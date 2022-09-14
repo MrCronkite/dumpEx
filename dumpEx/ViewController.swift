18168f4508cec6c8fa0e93025c470c366db87489
MrCronkite <vladshima99@gmail.com>
2022-09-14 22:41:24 +0300

    res

--- dumpEx/ViewController.swift
+++ dumpEx/ViewController.swift
@@ -8,11 +8,27 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    let firstView = UIView()
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        firstView.frame = CGRect(x: 10, y: 10, width: 50, height: 100)
+        firstView.backgroundColor = .systemGreen
+        
+        self.view.addSubview(firstView)
+        
     }
 
 
+}
+
+
+extension ViewController {
+    
+    
+    
+    
 }
 
