be1c04d8aa6e9fdffd6a7fc5f568e30db22fc5a6
MrCronkite <vladshima99@gmail.com>
2022-09-14 21:28:21 +0300

    new progect

---
+++ dumpEx/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  dumpEx
+//
+//  Created by admin1 on 14.09.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
