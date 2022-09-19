7fd8f52f1a0a1af948f7189428ef3c7780c1c902
MrCronkite <vladshima99@gmail.com>
2022-09-19 20:55:24 +0300

    settings

---
+++ dumpEx/Settings/SettingsView.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  dumpEx
+//
+//  Created by admin1 on 19.09.22.
+//
+
+import UIKit
+
+class SettingsView: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+}
