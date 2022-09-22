7da3b1a17c432ac9a89567e0673e7478049b3a2c
MrCronkite <vladshima99@gmail.com>
2022-09-22 21:22:46 +0300

    data picker

--- dumpEx/Settings/SettingsViewController.swift
+++ dumpEx/Settings/SettingsViewController.swift
@@ -9,6 +9,8 @@ import UIKit
 
 class SettingsViewController: UIViewController {
     
+    let myDatePicker: UIDatePicker = UIDatePicker()
+    
     @IBOutlet weak var dataButton: UIButton!
     @IBOutlet weak var dataLable: UILabel!
     @IBOutlet weak var mouthField: UITextField!
@@ -20,12 +22,28 @@ class SettingsViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        myDatePicker.datePickerMode = .date
+        myDatePicker.preferredDatePickerStyle = .wheels
+        myDatePicker.timeZone = TimeZone.current
+        //myDatePicker.calendar = .autoupdatingCurrent
+        myDatePicker.frame = CGRect(x: 0, y: 0, width: 270, height: 150)
+        
     }
+    
+  
     
 
     @IBAction func saveSettings(_ sender: Any) {
+        print(myDatePicker.date)
     }
     
     @IBAction func setData(_ sender: Any) {
+        let alertController = UIAlertController(title: "\n\n\n\n\n\n\n\n", message: nil, preferredStyle: .actionSheet)
+        alertController.view.addSubview(myDatePicker)
+        let somethingAction = UIAlertAction(title: "Ok", style: .default, handler: nil)
+        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
+        alertController.addAction(somethingAction)
+        alertController.addAction(cancelAction)
+        self.present(alertController, animated: true, completion:{})
     }
 }
