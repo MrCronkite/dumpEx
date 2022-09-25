687be8c6d187eee09d4c8a644503282787c1e829
MrCronkite <vladshima99@gmail.com>
2022-09-25 22:30:12 +0300

    data picker

--- dumpEx/ViewController.swift
+++ dumpEx/ViewController.swift
@@ -9,18 +9,36 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    
+    @IBOutlet weak var brakeTimeLabel: UILabel!
+    
+    @IBOutlet weak var moneyLabel: UILabel!
+    @IBOutlet weak var stackViewMoney: UIStackView!
+    @IBOutlet weak var stackViewTest: UIStackView!
+    @IBOutlet weak var currencyLabel: UILabel!
+    
+    @IBOutlet weak var buttonSettings: UIButton!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        stackViewMoney.layer.cornerRadius = 10
+        stackViewMoney.backgroundColor = .gray
+        
+        stackViewTest.layer.cornerRadius = 10
+        stackViewTest.backgroundColor = .gray
+        
+        buttonSettings.backgroundColor = .gray
+        buttonSettings.layer.cornerRadius = 10
+        
     }
     
-    @objc func showView(){
-      let storyboard = UIStoryboard(name: "SettingsViewController", bundle:  nil)
-      guard  let settingsVC = storyboard.instantiateViewController(withIdentifier: "SettingsViewController") as? SettingsViewController else { return}
-       present(settingsVC, animated: true, completion: nil)
-        
-        
+    
+    @IBAction func openSettingsView(_ sender: Any) {
+        let storyboard = UIStoryboard(name: "SettingsViewController", bundle:  nil)
+        guard  let settingsVC = storyboard.instantiateViewController(withIdentifier: "SettingsViewController") as? SettingsViewController else { return}
+         present(settingsVC, animated: true, completion: nil)
     }
 
 
