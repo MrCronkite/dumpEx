9cdf5ab08d171bf833d005de0d862aef922c88c8
MrCronkite <vladshima99@gmail.com>
2022-09-25 21:57:04 +0300

    data picker

--- dumpEx/ViewController.swift
+++ dumpEx/ViewController.swift
@@ -9,26 +9,9 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    let firstView = UIView()
-    let buttonView = UIButton()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        firstView.frame = CGRect(x: 10, y: 10, width: 50, height: 100)
-        firstView.backgroundColor = .systemGreen
-        
-        
-        buttonView.frame = CGRect(x: 100, y: 400, width: 100, height: 200)
-        buttonView.setTitle("Settings", for: .normal)
-        buttonView.setTitleColor(.blue, for: .normal)
-        buttonView.layer.cornerRadius = 20
-        buttonView.backgroundColor = .green
-        buttonView.layer.borderColor = .init(red: 10, green: 10, blue: 10, alpha: 10)
-        buttonView.addTarget(self, action: #selector(showView), for: .touchUpInside)
-        
-        self.view.addSubview(buttonView)
-        self.view.addSubview(firstView)
         
     }
     
