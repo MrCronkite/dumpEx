6d6f9bd0fde2de7305a7f370c3e012858df9f9bf
MrCronkite <vladshima99@gmail.com>
2022-10-04 21:33:40 +0300

    new action

---
+++ dumpEx/InfoPage/UsefulInformationViewController.swift
@@ -0,0 +1,18 @@
+//
+//  UsefulInformationViewController.swift
+//  dumpEx
+//
+//  Created by admin1 on 4.10.22.
+//
+
+import UIKit
+
+class UsefulInformationViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+}
