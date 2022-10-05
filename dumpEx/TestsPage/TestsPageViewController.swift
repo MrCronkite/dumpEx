7250a42a6f12b42e223983055aca566ebc95339f
MrCronkite <vladshima99@gmail.com>
2022-10-05 20:20:29 +0300

    new action

---
+++ dumpEx/TestsPage/TestsPageViewController.swift
@@ -0,0 +1,17 @@
+//
+//  TestsPageViewController.swift
+//  dumpEx
+//
+//  Created by admin1 on 5.10.22.
+//
+
+import UIKit
+
+class TestsPageViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+}
