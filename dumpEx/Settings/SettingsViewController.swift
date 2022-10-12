7f8c5f7532d01746d00b77715a8adfbf5c237436
MrCronkite <vladshima99@gmail.com>
2022-10-12 22:23:41 +0300

    add button callendar

--- dumpEx/Settings/SettingsViewController.swift
+++ dumpEx/Settings/SettingsViewController.swift
@@ -9,18 +9,19 @@ import UIKit
 
 class SettingsViewController: UIViewController {
     
-   // let myDatePicker: UIDatePicker = UIDatePicker()
+   let myDatePicker: UIDatePicker = UIDatePicker()
     
+   @IBOutlet weak var dataLabel: UILabel!
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
-//
-//        myDatePicker.datePickerMode = .date
-//        myDatePicker.preferredDatePickerStyle = .wheels
-//        myDatePicker.timeZone = TimeZone.current
-//        myDatePicker.frame = CGRect(x: 0, y: 0, width: 250 , height: 150)
-//
+
+        myDatePicker.datePickerMode = .date
+        myDatePicker.preferredDatePickerStyle = .wheels
+        myDatePicker.timeZone = TimeZone.current
+        myDatePicker.frame = CGRect(x: 0, y: 0, width: 250 , height: 150)
+
 //        mouthField.delegate = self
 //        expensesField.delegate = self
     }
@@ -40,20 +41,20 @@ class SettingsViewController: UIViewController {
 //        print("выбрать цену")
 //    }
 //
-//    @IBAction func setData(_ sender: Any) {
-//        let alertController = UIAlertController(title: "\n\n\n\n\n\n\n\n", message: nil, preferredStyle: .actionSheet)
-//        alertController.view.addSubview(myDatePicker)
-//        let somethingAction = UIAlertAction(title: "Ok", style: .default) { Picker in
-//            let formater = DateFormatter()
-//            formater.dateFormat = "yyyy.MM.dd"
-//            formater.timeZone = TimeZone.current
-//            self.dataLable.text = formater.string(from: self.myDatePicker.date)
-//        }
-//        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
-//        alertController.addAction(somethingAction)
-//        alertController.addAction(cancelAction)
-//        self.present(alertController, animated: true, completion:{})
-//    }
+    @IBAction func setData(_ sender: Any) {
+        let alertController = UIAlertController(title: "\n\n\n\n\n\n\n\n", message: nil, preferredStyle: .actionSheet)
+        alertController.view.addSubview(myDatePicker)
+        let somethingAction = UIAlertAction(title: "Ok", style: .default) { Picker in
+            let formater = DateFormatter()
+            formater.dateFormat = "yyyy.MM.dd"
+            formater.timeZone = TimeZone.current
+            self.dataLabel.text = formater.string(from: self.myDatePicker.date)
+        }
+        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
+        alertController.addAction(somethingAction)
+        alertController.addAction(cancelAction)
+        self.present(alertController, animated: true, completion:{})
+    }
 }
 
 
