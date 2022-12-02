fb7475cc8f42aeae996a407f8adae39eed1bdeb0
MrCronkite <vladshima99@gmail.com>
2022-12-02 22:29:54 +0300

    add new field

--- dumpEx/EmotionalConditionPage/EmotionalConditionViewController.swift
+++ dumpEx/EmotionalConditionPage/EmotionalConditionViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 class EmotionalConditionViewController: UIViewController {
     
     let indetifire = "keyCell"
-    let tableCellName = ["Эмоциональное состояние", "День 1", "День 2", "День 3", "День 4", "День 5","День 6","День 7","День 8", "День 9", "День 10"]
+    let tableCellName = ["Ваше Эмоциональное состояние", "День 1", "День 2", "День 3", "День 4", "День 5","День 6","День 7","День 8", "День 9", "День 10", "Неделя 2", "Неделя 3", "Неделя 4", "Неделя 5", "2 Месяца", "3 Месяца", "4 месяца", "Полгода!"]
 
     @IBOutlet weak var tableContainer: UITableView!
     
